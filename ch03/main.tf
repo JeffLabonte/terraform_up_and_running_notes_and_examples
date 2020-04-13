45de1a3bdf05e907586b4563555a037e45b73421
Jeff Labonte <jeff.labonte12@gmail.com>
2020-04-13 17:10:06 -0400

    Listen to 80, if not matching listener rules return 404

--- ch03/main.tf
+++ ch03/main.tf
@@ -63,9 +63,24 @@ resource "aws_lb" "example" {
     subnets            = data.aws_subnet_ids.default.ids
 }
 
+resource "aws_lib_listener" "http" {    
+    load_balancer_arn = aws_lib.example.arn
+    port              = 80
+    protocol          = "http"
+    
+    # By default, return a simple 404 page
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content-type = "text/plain"
+            message_body = "404; page not found"
+            status_code  = 404
+        }
+    }
+}
 
 output "public_ip" {
   value = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
-
