32b61bf49769271b8dbde6a5f1cba80332294f1f
Jeff Labonte <jeff.labonte12@gmail.com>
2020-04-12 20:55:28 -0400

    Add output once command is done

--- ch03/main.tf
+++ ch03/main.tf
@@ -35,3 +35,9 @@ resource "aws_security_group" "instance" {
     }
   
 }
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
