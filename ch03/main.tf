5ac10f52fbeebadf0ffb46b67a514323128e99c7
Jeff Labonte <jeff.labonte12@gmail.com>
2020-04-13 17:29:39 -0400

    Add load balancer target group with health status

--- ch03/main.tf
+++ ch03/main.tf
@@ -61,6 +61,7 @@ resource "aws_lb" "example" {
     name               = "terraform-asg-example"
     load_balancer_type = "application"
     subnets            = data.aws_subnet_ids.default.ids
+    security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lib_listener" "http" {    
@@ -79,6 +80,42 @@ resource "aws_lib_listener" "http" {
         }
     }
 }
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name     = "terraform-asg-example"
+    port     = var.server-port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path                = "/"
+        protocol            = "HTTP"
+        matcher             = "200"
+        interval            = 15
+        timeout             = 3
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
+}
+
 
 output "public_ip" {
   value = aws_instance.example.public_ip
